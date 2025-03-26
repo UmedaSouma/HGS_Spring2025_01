4530407bc567db464f84cdc3c800b5ad85e52218
gotouyuuki0208 <231103@st.yoshida-g.ac.jp>
2025-03-26 10:33:01 +0900

    ゴールのモデル出せる

---
+++ goal.cpp
@@ -0,0 +1,103 @@
+//==========================
+// 
+// ゴール[goal.cpp]
+// Author Yuki Goto
+//
+//==========================
+
+//include
+#include "goal.h"
+#include "manager.h"
+#include "model.h"
+
+//静的メンバ初期化
+const int CGoal::PRIORITY = 1;//描画順
+
+//==========================
+//コンストラクタ
+//==========================
+CGoal::CGoal(int nPriority) :
+	CObjectX(nPriority),//基底コンストラクタ
+	m_nModelIdx(0)//モデルの番号
+{
+
+}
+
+//==========================
+//デストラクタ
+//==========================
+CGoal::~CGoal()
+{
+
+}
+
+//==========================
+//初期化処理
+//==========================
+HRESULT CGoal::Init()
+{
+	//サイズ設定
+	//SetSize();
+
+	//初期設定
+	CObjectX::Init();
+
+	return S_OK;
+}
+
+//==========================
+//終了処理
+//==========================
+void  CGoal::Uninit()
+{
+	//終了処理
+	CObjectX::Uninit();
+
+	Release();
+}
+
+//==========================
+//更新処理
+//==========================
+void CGoal::Update()
+{
+	//更新処理
+	CObjectX::Update();
+}
+
+//==========================
+//描画処理
+//==========================
+void CGoal::Draw()
+{
+	//描画処理
+	CObjectX::Draw();
+}
+
+//==========================
+//オブジェクト生成
+//==========================
+CGoal* CGoal::Create(D3DXVECTOR3 pos, D3DXVECTOR3 scale)
+{
+	//インスタンス生成
+	CGoal* pGoal = DBG_NEW CGoal;
+
+	//位置の設定
+	pGoal->SetPos(pos);
+
+	//スケールの設定
+	pGoal->SetScale(scale);
+
+	//モデルの生成
+	CModel* pModel = CManager::GetInstance()->GetModel();
+	pGoal->m_nModelIdx = pModel->Regist("data\\MODEL\\soil.x");
+	pGoal->BindModel(pModel->GetMeshAdress(pGoal->m_nModelIdx),
+		pModel->GetBuffMatAdress(pGoal->m_nModelIdx),
+		pModel->GetNumMatAdress(pGoal->m_nModelIdx),
+		pModel->GetTexIdx(pGoal->m_nModelIdx));
+
+	//初期化処理
+	pGoal->Init();
+
+	return pGoal;
+}
--- goal.h
+++ goal.h
@@ -1,1 +1,31 @@
-#pragma once
+//==========================
+// 
+// ゴール[goal.h]
+// Author Yuki Goto
+//
+//==========================
+#ifndef _GOAL_H_
+#define _GOAL_H_
+
+//include
+#include "objectX.h"
+
+//クラスの定義
+class CGoal :public CObjectX
+{
+public:
+	//定数
+	static const int PRIORITY;//描画順
+
+	//メンバ関数
+	CGoal(int nPriority = PRIORITY);//コンストラクタ
+	~CGoal() override;//デストラクタ
+	HRESULT Init() override;//初期化処理
+	void Uninit() override;//終了処理
+	void Update() override;//更新処理
+	void Draw() override;//描画処理
+	static CGoal* Create(D3DXVECTOR3 pos, D3DXVECTOR3 scale);//オブジェクト生成
+private:
+	int m_nModelIdx;//モデルの番号
+};
+#endif
